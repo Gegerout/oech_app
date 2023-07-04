0afe59df4f42eb80e0096ba9ba2cec7e70ee4303
Gegerout <selimchik06@gmail.com>
2023-07-04 19:44:51 +0300

    session-4

---
+++ lib/core/states/network_state.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/foundation.dart';
+
+enum NetworkStatus { online, offline }
+
+class NetworkService extends ChangeNotifier {
+  StreamController<NetworkStatus> controller = StreamController();
+
+  NetworkService() {
+    Connectivity().onConnectivityChanged.listen((event) {
+      controller.add(_networkStatus(event));
+    });
+  }
+
+  NetworkStatus _networkStatus(ConnectivityResult connectivityResult) {
+    return connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi
+        ? NetworkStatus.online
+        : NetworkStatus.offline;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,20 @@ import 'package:oech_app/home/presentation/pages/home_page.dart';
 import 'package:oech_app/onboarding/presentation/pages/onboarding_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:stack_trace/stack_trace.dart' as stack_trace;
+import 'package:provider/provider.dart' as provider;
 
 import 'auth/presentation/pages/signup_page.dart';
+import 'core/states/network_state.dart';
 
 Future<void> main() async {
   await Supabase.initialize(
       url: "https://feyqfihsyhchwsjseqsg.supabase.co",
       anonKey:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZleXFmaWhzeWhjaHdzanNlcXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc4ODQ0NjQsImV4cCI6MjAwMzQ2MDQ2NH0.iIjNJGoJ08Gyqo8C1OHG3nHw7CtNRFtGmcLQq10qTxo");
-  runApp(ProviderScope(child: MyApp()));
+  runApp(provider.MultiProvider(providers: [
+    provider.ChangeNotifierProvider(
+      create: (context) => NetworkService(),),
+  ], child: ProviderScope(child: MyApp())));
   FlutterError.demangleStackTrace = (StackTrace stack) {
     if (stack is stack_trace.Trace) return stack.vmTrace;
     if (stack is stack_trace.Chain) return stack.toTrace().vmTrace;
@@ -28,6 +33,8 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var networkStatus = context.read<NetworkService>();
+
     return MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
@@ -39,27 +46,49 @@ class MyApp extends ConsumerWidget {
             colorScheme:
             ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
             fontFamily: "Roboto"),
-        home: ref.watch(mainProvider).when(
-            data: (value) {
-              if (value[0] && !value[1] && !(supabase.auth.currentUser != null && supabase.auth.currentUser?.appMetadata["provider"] != "email")) {
-                return SignupPage();
-              }
-              else if(value[1] || supabase.auth.currentUser != null) {
-                return const HomePage(0);
-              }
-              return const OnboardingPage();
-            },
-            error: (error, stacktrace) {
-              return Scaffold(
+        home: StreamBuilder(
+          builder: (context, snapshot) {
+            if(snapshot.data == NetworkStatus.offline) {
+              WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text("No internet connection"),
+                      actions: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text("Ok"))
+                      ],
+                    )
+                );
+              });
+            }
+
+            return ref.watch(mainProvider).when(
+              data: (value) {
+                if (value[0] && !value[1] && !(supabase.auth.currentUser != null && supabase.auth.currentUser?.appMetadata["provider"] != "email")) {
+                  return SignupPage();
+                }
+                else if(value[1] || supabase.auth.currentUser != null) {
+                  return const HomePage(0);
+                }
+                return const OnboardingPage();
+              },
+              error: (error, stacktrace) {
+                return Scaffold(
+                  body: Center(
+                    child: Text(error.toString()),
+                  ),
+                );
+              },
+              loading: () => const Scaffold(
                 body: Center(
-                  child: Text(error.toString()),
+                  child: CircularProgressIndicator(),
                 ),
-              );
-            },
-            loading: () => const Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
-            )));
+              ));
+          }
+        ));
   }
 }
