24000fec1d9f65167dfbf8955d58ddeebc173c84
Gegerout <selimchik06@gmail.com>
2023-07-04 19:39:15 +0300

    session-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:oech_app/core/states/main_state.dart';
+import 'package:oech_app/core/states/network_state.dart';
 import 'package:oech_app/core/theme/colors.dart';
 import 'package:oech_app/home/data/data_sources/remote_data.dart';
 import 'package:oech_app/home/presentation/pages/home_page.dart';
@@ -14,8 +15,10 @@ Future<void> main() async {
   await Supabase.initialize(
       url: "https://feyqfihsyhchwsjseqsg.supabase.co",
       anonKey:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZleXFmaWhzeWhjaHdzanNlcXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc4ODQ0NjQsImV4cCI6MjAwMzQ2MDQ2NH0.iIjNJGoJ08Gyqo8C1OHG3nHw7CtNRFtGmcLQq10qTxo");
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZleXFmaWhzeWhjaHdzanNlcXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc4ODQ0NjQsImV4cCI6MjAwMzQ2MDQ2NH0.iIjNJGoJ08Gyqo8C1OHG3nHw7CtNRFtGmcLQq10qTxo");
   runApp(provider.MultiProvider(providers: [
+    provider.ChangeNotifierProvider(
+        create: (context) => NetworkService(),),
     provider.ChangeNotifierProvider<RemoteData>(
       create: (_) => RemoteData(),
     )
@@ -29,42 +32,68 @@ Future<void> main() async {
 
 class MyApp extends ConsumerWidget {
   MyApp({super.key});
+
   final supabase = Supabase.instance.client;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var networkStatus = context.read<NetworkService>();
+
     return MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
             scaffoldBackgroundColor: AppColors.scaffoldColor,
-            appBarTheme: const AppBarTheme(
-                elevation: 3.3
-            ),
+            appBarTheme: const AppBarTheme(elevation: 3.3),
             colorScheme:
-            ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
+                ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
             fontFamily: "Roboto"),
-        home: ref.watch(mainProvider).when(
-            data: (value) {
-              if (value[0] && !value[1] && !(supabase.auth.currentUser != null && supabase.auth.currentUser?.appMetadata["provider"] != "email")) {
-                return SignupPage();
-              }
-              else if(value[1] || supabase.auth.currentUser != null) {
-                return const HomePage(0);
-              }
-              return const OnboardingPage();
-            },
-            error: (error, stacktrace) {
-              return Scaffold(
-                body: Center(
-                  child: Text(error.toString()),
-                ),
-              );
-            },
-            loading: () => const Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
-            )));
+        home: StreamBuilder(
+          stream: networkStatus.controller.stream,
+          builder: (context, snapshot) {
+            if(snapshot.data == NetworkStatus.offline) {
+              WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text("No internet connection"),
+                      actions: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text("Ok"))
+                      ],
+                    )
+                );
+              });
+            }
+            return ref.watch(mainProvider).when(
+                data: (value) {
+                  if (value[0] &&
+                      !value[1] &&
+                      !(supabase.auth.currentUser != null &&
+                          supabase.auth.currentUser?.appMetadata["provider"] !=
+                              "email")) {
+                    return SignupPage();
+                  } else if (value[1] || supabase.auth.currentUser != null) {
+                    return const HomePage(0);
+                  }
+                  return const OnboardingPage();
+                },
+                error: (error, stacktrace) {
+                  return Scaffold(
+                    body: Center(
+                      child: Text(error.toString()),
+                    ),
+                  );
+                },
+                loading: () => const Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ));
+          },
+        ));
   }
 }
